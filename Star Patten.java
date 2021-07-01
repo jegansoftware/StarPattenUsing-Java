c0e28049cb469ecbc1c0c5d0c84c4b3d9cfd9647
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-07-01 20:37:14 +0530

    Create Star Patten.java

---
+++ Star Patten.java
@@ -0,0 +1,23 @@
+public class main{
+public static void main(String[] args)
+{
+int i;
+int j;
+
+n=10;
+
+    for(i=1;i<=n;i++)
+    {
+          for(j=1;j<n-i;j++)
+          {
+            System.out.printf(" ");
+          }
+
+          for(j=1;j<i;j++)
+          {
+            Systsm.out.printf("*");
+          }
+    }
+
+}
+}
